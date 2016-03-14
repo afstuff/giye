6217fd98bb02d6fc5589b49e09999e8c87efedeb
james <afrecruits@yahoo.com>
2016-03-14 20:29:01 +0100

    the login profiles switched on for grouplife

--- ABS_GLIFE/LoginP.aspx.vb
+++ ABS_GLIFE/LoginP.aspx.vb
@@ -23,7 +23,7 @@ Partial Class LoginP
 
     Dim sUsername As String = ""
     Dim sPassword As String = ""
-
+    Dim strSQL As String
     Dim lrcValidate As String
     Dim strPWD As String
 
@@ -168,54 +168,119 @@ Partial Class LoginP
     End Sub
 
     Protected Sub LoginBtn_Click(ByVal sender As Object, ByVal e As EventArgs) Handles LoginBtn.Click
+        lblMessage.Text = ""
+        Dim mystrCONN_Chk As String = ""
 
-        ' get required user parameters
-        lrcValidate = ""
+        Dim objOLEConn_Chk As OleDbConnection = Nothing
+        Dim objOLECmd_Chk As OleDbCommand = Nothing
+        Dim objOLEDR_Chk As OleDbDataReader
 
-        sUsername = txtUserID.Text
-        sPassword = txtUser_PWD.Text
+        Dim myTmp_Chk As String
+        Dim myTmp_Ref As String
+        myTmp_Chk = "N"
+        myTmp_Ref = ""
+
+
+        mystrCONN_Chk = CType(Session("connstr"), String)
+        objOLEConn_Chk = New OleDbConnection()
+        objOLEConn_Chk.ConnectionString = mystrCONN_Chk
 
         Try
-            ' try and authenticate the user
-            lrcValidate = Me.ValidateUserLogin(sUsername, sPassword)
-            ' test the valid return code of the user authentication
-            If lrcValidate = "True" Then
-                Session.Add("MyLogonTime", System.DateTime.Now)
-                Session.Add("MyUserID", sUsername)
-                Dim strUserName As String = "User Name: " & CType(Session("MyUserName"), String)
-                lblMessage.Text = strUserName
+            'open connection to database
+            objOLEConn_Chk.Open()
+        Catch ex As Exception
+            lblMessage.Text = "Unable to connect to database. Reason: " & ex.Message
+            'FirstMsg = "Javascript:alert('" & Me.txtMsg.Text & "')"
+            objOLEConn_Chk = Nothing
+            Exit Sub
+        End Try
 
-                'Credentials are ok, redirect back to the page that forced
-                'authentication, pass the user name and don't persist the cookie
+        Try
+            Dim User_Login = Trim(txtUserID.Text)
+            Dim User_Password = Trim(EncryptNew(txtUser_PWD.Text))
+            'strSQL = "SELECT * FROM SEC_USER_LIFE_DETAIL WHERE SEC_USER_LOGIN=@pUser_Login " & _
+            '         "and SEC_USER_PASSWORD=@pUser_Password"
+            strSQL = "SELECT * FROM SEC_USER_LIFE_DETAIL WHERE SEC_USER_LOGIN='" & User_Login & "' " & _
+                   "and SEC_USER_PASSWORD='" & User_Password & "' "
+            objOLECmd_Chk = New OleDbCommand(strSQL, objOLEConn_Chk)
+            objOLECmd_Chk.CommandType = CommandType.Text
 
-                'System.Web.Security.FormsAuthentication.RedirectFromLoginPage(txtUserID.Text, False)
-
-
-                'Response.Redirect("absMain.aspx")
-                'Response.Redirect(Request.ApplicationPath & "/UNP_FRA.aspx")
-                'Response.Redirect(Request.ApplicationPath & "/UNP_MENU.aspx")
-                'Response.Redirect(Request.ApplicationPath & "/UNP_MNU.aspx")
-
+            'objOLECmd_Chk.Parameters.AddWithValue("pUser_Login", Trim(txtUserID.Text))
+            objOLEDR_Chk = objOLECmd_Chk.ExecuteReader()
+            If (objOLEDR_Chk.Read()) Then
+                Session("MyUserIDX") = Trim(Me.txtUserID.Text)
+                'Session("MyUserName") = UCase(Me.txtUserName.Text)
+                Session("MyUserName") = objOLEDR_Chk("SEC_USER_NAME")
+                Session("MyUserRole") = objOLEDR_Chk("SEC_USER_ROLE")
                 If Request.QueryString("Goto") <> "" Then
                     Response.Redirect(Request.QueryString("Goto"))
                 Else
-                    'Response.Redirect("M_MENU.aspx?menu=HOME")
                     Response.Redirect("MENU_GL.aspx?menu=HOME")
-
                 End If
-
-            ElseIf lrcValidate = "Invalid_User" Then
-                lblMessage.Text = "Invalid UserName and Password."
-            ElseIf lrcValidate = "Invalid_Password" Then
-                lblMessage.Text = "Password Incorrect, Please try again."
             Else
-                lblMessage.Text = "Error(s) Occured!." & "<BR>" & lrcValidate & "<BR>" & "Unable to Authenticate User at this time."
+                Me.lblMessage.Text = "Login information is not correct. Enter Valid User ID and Password..."
+                Me.txtUserID.Enabled = True
+                Me.txtUserID.Focus()
+                Exit Sub
             End If
-
         Catch ex As Exception
-            lblMessage.Text = "Unable to Authenticate User at this time."
-
+            Me.lblMessage.Text = "Error has occured. Reason: " & ex.Message.ToString()
         End Try
+        objOLEDR_Chk = Nothing
+        objOLECmd_Chk.Dispose()
+        objOLECmd_Chk = Nothing
+        If objOLEConn_Chk.State = ConnectionState.Open Then
+            objOLEConn_Chk.Close()
+        End If
+        objOLEConn_Chk = Nothing
+
+        '' get required user parameters
+        'lrcValidate = ""
+
+        'sUsername = txtUserID.Text
+        'sPassword = txtUser_PWD.Text
+
+        'Try
+        '    ' try and authenticate the user
+        '    lrcValidate = Me.ValidateUserLogin(sUsername, sPassword)
+        '    ' test the valid return code of the user authentication
+        '    If lrcValidate = "True" Then
+        '        Session.Add("MyLogonTime", System.DateTime.Now)
+        '        Session.Add("MyUserID", sUsername)
+        '        Dim strUserName As String = "User Name: " & CType(Session("MyUserName"), String)
+        '        lblMessage.Text = strUserName
+
+        '        'Credentials are ok, redirect back to the page that forced
+        '        'authentication, pass the user name and don't persist the cookie
+
+        '        'System.Web.Security.FormsAuthentication.RedirectFromLoginPage(txtUserID.Text, False)
+
+
+        '        'Response.Redirect("absMain.aspx")
+        '        'Response.Redirect(Request.ApplicationPath & "/UNP_FRA.aspx")
+        '        'Response.Redirect(Request.ApplicationPath & "/UNP_MENU.aspx")
+        '        'Response.Redirect(Request.ApplicationPath & "/UNP_MNU.aspx")
+
+        '        If Request.QueryString("Goto") <> "" Then
+        '            Response.Redirect(Request.QueryString("Goto"))
+        '        Else
+        '            'Response.Redirect("M_MENU.aspx?menu=HOME")
+        '            Response.Redirect("MENU_GL.aspx?menu=HOME")
+
+        '        End If
+
+        '    ElseIf lrcValidate = "Invalid_User" Then
+        '        lblMessage.Text = "Invalid UserName and Password."
+        '    ElseIf lrcValidate = "Invalid_Password" Then
+        '        lblMessage.Text = "Password Incorrect, Please try again."
+        '    Else
+        '        lblMessage.Text = "Error(s) Occured!." & "<BR>" & lrcValidate & "<BR>" & "Unable to Authenticate User at this time."
+        '    End If
+
+        'Catch ex As Exception
+        '    lblMessage.Text = "Unable to Authenticate User at this time."
+
+        'End Try
 
 
         'If Me.txtUserID.Text = "CRU" And Me.txtUser_PWD.Text = "CRU*123" Then
@@ -259,35 +324,90 @@ Partial Class LoginP
     End Sub
 
     Protected Sub txtUserID_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtUserID.TextChanged
+
+        lblMessage.Text = ""
+        Dim mystrCONN_Chk As String = ""
+
+        Dim objOLEConn_Chk As OleDbConnection = Nothing
+        Dim objOLECmd_Chk As OleDbCommand = Nothing
+        Dim objOLEDR_Chk As OleDbDataReader
+
+        Dim myTmp_Chk As String
+        Dim myTmp_Ref As String
+        myTmp_Chk = "N"
+        myTmp_Ref = ""
+
+
+        mystrCONN_Chk = CType(Session("connstr"), String)
+        objOLEConn_Chk = New OleDbConnection()
+        objOLEConn_Chk.ConnectionString = mystrCONN_Chk
+
+        Try
+            'open connection to database
+            objOLEConn_Chk.Open()
+        Catch ex As Exception
+            lblMessage.Text = "Unable to connect to database. Reason: " & ex.Message
+            objOLEConn_Chk = Nothing
+            Exit Sub
+        End Try
+
+        Try
+            Dim User_Login = Trim(txtUserID.Text)
+            strSQL = "SELECT * FROM SEC_USER_LIFE_DETAIL WHERE SEC_USER_LOGIN='" & User_Login & "'"
+            objOLECmd_Chk = New OleDbCommand(strSQL, objOLEConn_Chk)
+            objOLECmd_Chk.CommandType = CommandType.Text
+            objOLEDR_Chk = objOLECmd_Chk.ExecuteReader()
+            If (objOLEDR_Chk.Read()) Then
+                Session("MyUserIDX") = Trim(Me.txtUserID.Text)
+                txtUserName.Text = objOLEDR_Chk("SEC_USER_NAME")
+            Else
+                Me.lblMessage.Text = "User ID does not exist"
+                txtUserName.Text = ""
+                Me.txtUserID.Enabled = True
+                Me.txtUserID.Focus()
+                Exit Sub
+            End If
+        Catch ex As Exception
+            Me.lblMessage.Text = "Error has occured. Reason: " & ex.Message.ToString()
+        End Try
+        objOLEDR_Chk = Nothing
+        objOLECmd_Chk.Dispose()
+        objOLECmd_Chk = Nothing
+        If objOLEConn_Chk.State = ConnectionState.Open Then
+            objOLEConn_Chk.Close()
+        End If
+        objOLEConn_Chk = Nothing
+
+
         ' try and authenticate the user
 
-        sUsername = txtUserID.Text
-        sPassword = txtUser_PWD.Text
+        'sUsername = txtUserID.Text
+        'sPassword = txtUser_PWD.Text
 
-        lrcValidate = Me.ValidateUserLogin(sUsername, sPassword)
+        'lrcValidate = Me.ValidateUserLogin(sUsername, sPassword)
 
-        Me.txtUser_PWD.Enabled = True
-        Me.txtUser_PWD.Focus()
+        'Me.txtUser_PWD.Enabled = True
+        'Me.txtUser_PWD.Focus()
 
-        'Select Case Trim(Me.txtUserID.Text)
-        '    Case "CRU"
-        '        Me.txtUserName.Text = "System Administrator"
-        '        Me.txtUser_PWD.Enabled = True
-        '        Me.txtUser_PWD.Focus()
-        '    Case "user1"
-        '        Me.txtUserName.Text = UCase("Life User 1")
-        '        Me.txtUser_PWD.Enabled = True
-        '        Me.txtUser_PWD.Focus()
-        '    Case "user2"
-        '        Me.txtUserName.Text = UCase("Life User 2")
-        '        Me.txtUser_PWD.Enabled = True
-        '        Me.txtUser_PWD.Focus()
-        '    Case "user3"
-        '        Me.txtUserName.Text = UCase("Life User 3")
-        '        Me.txtUser_PWD.Enabled = True
-        '        Me.txtUser_PWD.Focus()
-        '    Case Else
-        'End Select
+        ''Select Case Trim(Me.txtUserID.Text)
+        ''    Case "CRU"
+        ''        Me.txtUserName.Text = "System Administrator"
+        ''        Me.txtUser_PWD.Enabled = True
+        ''        Me.txtUser_PWD.Focus()
+        ''    Case "user1"
+        ''        Me.txtUserName.Text = UCase("Life User 1")
+        ''        Me.txtUser_PWD.Enabled = True
+        ''        Me.txtUser_PWD.Focus()
+        ''    Case "user2"
+        ''        Me.txtUserName.Text = UCase("Life User 2")
+        ''        Me.txtUser_PWD.Enabled = True
+        ''        Me.txtUser_PWD.Focus()
+        ''    Case "user3"
+        ''        Me.txtUserName.Text = UCase("Life User 3")
+        ''        Me.txtUser_PWD.Enabled = True
+        ''        Me.txtUser_PWD.Focus()
+        ''    Case Else
+        ''End Select
 
     End Sub
 
